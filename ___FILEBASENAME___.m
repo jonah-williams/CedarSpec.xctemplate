1a0e0c6b2f37f1518e7033699f953079403ffbcb
Jonah Williams <jonah@carbonfive.com>
2011-08-12 17:31:40 -0700

    Cedar spec file template

---
+++ ___FILEBASENAME___.m
@@ -0,0 +1,30 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <Cedar-iPhone/SpecHelper.h>
+#define HC_SHORTHAND
+#import <OCHamcrest-iPhone/OCHamcrest.h>
+#import <OCMock-iPhone/OCMock.h>
+
+SPEC_BEGIN(___VARIABLE_specSubject___Spec)
+
+describe(@"___VARIABLE_specSubject___", ^{
+	beforeEach(^{
+		//before each content
+	});
+
+	afterEach(^{
+		//after each content
+	});
+
+	it(@"should ", ^{
+		//expected behavior content
+	});
+});
+
+SPEC_END
